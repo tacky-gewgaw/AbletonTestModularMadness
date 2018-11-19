aeb4b3ed5034317b442761b3012d5bd95d2b07e8
Sander <sander@hiro.nl>
2018-11-19 20:58:54 +0100

    Initial Commit

---
+++ AbletonTestModularMadness/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AbletonTestModularMadness
+//
+//  Created by Sander van Kasteel on 19/11/2018.
+//  Copyright © 2018 Sander van Kasteel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
