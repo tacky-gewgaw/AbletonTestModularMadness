133fde1ab9c733a3fb3a7e769cb534a9cbc2d4f5
Sander <sander@hiro.nl>
2018-12-03 10:53:13 +0100

    Add tests for individual Module classes

--- ModularMadnessTests/test_modules.cpp
+++ ModularMadnessTests/test_modules.cpp
@@ -7,3 +7,113 @@
 //
 
 #include <stdio.h>
+#include <string>
+#include "catch.hpp"
+#include "../ModularMadness/Model/Module/NoopModule.cpp"
+#include "../ModularMadness/Model/Module/DelayModule.cpp"
+#include "../ModularMadness/Model/Module/EchoModule.cpp"
+#include "../ModularMadness/Model/Module/ReverseModule.cpp"
+
+const std::vector<std::string> inputStrings = {"do", "re", "mi", "fa", "sol", "la", "ti"};
+
+TEST_CASE("Any Module inheriting from BaseModule generates its input as an in order concatenation of the outputs of its predecessing Modules", "[base-module]") {
+    BaseModule* m = new NoopModule();
+    
+    const std::string expectedResult = "doremifasollati";
+    std::vector<BaseModule*> outputModules = {
+        new NoopModule(),
+        new NoopModule(),
+        new NoopModule(),
+        new NoopModule(),
+        new NoopModule(),
+        new NoopModule(),
+        new NoopModule()
+    };
+    
+    int i = 0;
+    for (BaseModule* o : outputModules) {
+        std::string input = inputStrings[i];
+        o -> overrideInput(input);
+        o -> generateOutput();
+        m -> connnectToOutputOf(o);
+        i++;
+    }
+    
+    m -> generateOutput();
+    
+    std::string actualResult;
+    m -> getOutput(actualResult);
+    
+    REQUIRE(expectedResult == actualResult);
+    
+    for (BaseModule* o : outputModules) {
+        delete o;
+    }
+    delete m;
+    
+    outputModules.clear();
+    
+    actualResult.clear();
+}
+
+TEST_CASE("The output of a NoopModule equals its input", "[noop-module]") {
+    NoopModule* noopModule = new NoopModule();
+    
+    for (std::string s : inputStrings) {
+        noopModule -> overrideInput(s);
+        noopModule -> generateOutput();
+        std::string result;
+        noopModule -> getOutput(result);
+        REQUIRE(s == result);
+    }
+    
+    delete noopModule;
+}
+
+TEST_CASE("The output of a ReverseModule equals the reverse of its input", "[reverse-module]") {
+    ReverseModule* reverseModule = new ReverseModule();
+    
+    const std::vector<std::string> expectedOutputStrings = {"od", "er", "im", "af", "los", "al", "it"};
+    
+    for (int i = 0; i < inputStrings.size(); i++) {
+        reverseModule -> overrideInput(inputStrings[i]);
+        reverseModule -> generateOutput();
+        std::string result;
+        reverseModule -> getOutput(result);
+        REQUIRE(expectedOutputStrings[i] == result);
+    }
+    
+    delete reverseModule;
+}
+
+TEST_CASE("The output of a DelayModule equals its previous input", "[delay-module]") {
+    DelayModule* delayModule = new DelayModule();
+    
+    const std::vector<std::string> expectedOutputStrings = {"hello", "do", "re", "mi", "fa", "sol", "la"};
+    
+    for (int i = 0; i < inputStrings.size(); i++) {
+        delayModule -> overrideInput(inputStrings[i]);
+        delayModule -> generateOutput();
+        std::string result;
+        delayModule -> getOutput(result);
+        REQUIRE(expectedOutputStrings[i] == result);
+    }
+    
+    delete delayModule;
+}
+
+TEST_CASE("The output of an EchoModule equals the a concatenation of the input with itself", "[echo-module]") {
+    EchoModule* echoModule = new EchoModule();
+    
+    const std::vector<std::string> expectedOutputStrings = {"dodo", "rere", "mimi", "fafa", "solsol", "lala", "titi"};
+    
+    for (int i = 0; i < inputStrings.size(); i++) {
+        echoModule -> overrideInput(inputStrings[i]);
+        echoModule -> generateOutput();
+        std::string result;
+        echoModule -> getOutput(result);
+        REQUIRE(expectedOutputStrings[i] == result);
+    }
+    
+    delete echoModule;
+}
